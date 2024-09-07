f534cd6d0e29db8ccc27759caa2d0536d197e54d
Adam Ziolkowski <zett.adam@gmail.com>
2024-09-07 18:55:33 -0500

    Add first meta description tag

--- src/features/home/HomeRoute.tsx
+++ src/features/home/HomeRoute.tsx
@@ -1,5 +1,5 @@
 import type { Component, JSX } from 'solid-js'
-import { Title } from '@solidjs/meta'
+import { Meta, Title } from '@solidjs/meta'
 
 import AboutMe from './AboutMe'
 import Quotation from './Quotation'
@@ -10,6 +10,10 @@ const HomeRoute: Component = (): JSX.Element => {
   return (
     <>
       <Title>Home — Adam Ziolkowski</Title>
+      <Meta
+        name="description"
+        content="You have arrived at the place Adam Ziolkowski calls home. Welcome friend and enter."
+      />
       <div class="home">
         <AboutMe />
         <Quotation />
