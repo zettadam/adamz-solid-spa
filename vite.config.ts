d7fa2d1915c9928ad008e4ba742ce0297bbc11f7
Adam Ziolkowski <zett.adam@gmail.com>
2024-09-19 05:46:51 -0500

    Add links to preload background images

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-// import prefetchPlugin from 'vite-plugin-bundle-prefetch'
-// import webFontDownload from 'vite-plugin-webfont-dl'
 
 export default defineConfig({
-  plugins: [
-    solid(),
-    // prefetchPlugin(),
-    //webFontDownload()
-  ],
+  plugins: [solid()],
   resolve: {
     alias: {
       '~/': new URL('./src/', import.meta.url).pathname,
